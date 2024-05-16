094388508ae6247199d77c23d89b986eac9639a6
Your Name <you@example.com>
2024-05-16 11:01:50 +0800

    跨域

---
+++ src/main/java/top/tsview/cli/config/WebConfig.java
@@ -0,0 +1,21 @@
+package top.tsview.cli.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**") // 匹配所有请求路径
+				.allowedOrigins("*") // 允许所有源
+				.allowedMethods("*") // 允许跨域的RESTFUL
+				.allowedHeaders("*") // 允许所有请求头
+				.allowCredentials(false) // 是否允许携带cookie
+				.maxAge(3600); // 预检请求的有效期，单位秒
+	}
+
+}
+
